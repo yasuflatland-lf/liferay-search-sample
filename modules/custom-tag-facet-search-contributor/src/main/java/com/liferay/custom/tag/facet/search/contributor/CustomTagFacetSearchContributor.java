da58f1de9b2c656f3fde097a11098913edc1d61d
Gabriel Lee <gabriel.lee@liferay.com>
2020-06-05 09:15:17 -0700

    Create CustomTagFacetContributor

---
+++ modules/custom-tag-facet-search-contributor/src/main/java/com/liferay/custom/tag/facet/search/contributor/CustomTagFacetSearchContributor.java
@@ -0,0 +1,21 @@
+package com.liferay.custom.tag.facet.search.contributor;
+
+import TagFacetSearchContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author gabriel
+ */
+@Component(
+	immediate = true,
+	property = {
+		// TODO enter required service properties
+	},
+	service = TagFacetSearchContributor.class
+)
+public class CustomTagFacetSearchContributor implements TagFacetSearchContributor {
+
+	// TODO enter required service methods
+
+}
