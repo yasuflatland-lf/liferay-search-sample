12b5875ffc494a38e9a816ecf484dfff59693607
Gabriel Lee <gabriel.lee@liferay.com>
2020-07-22 08:24:18 -0700

    Add Function Score

--- modules/custom-order-search-contributor-portlet/src/main/java/custom/order/search/contributor/CustomOrderSharedSearchContributor.java
+++ modules/custom-order-search-contributor-portlet/src/main/java/custom/order/search/contributor/CustomOrderSharedSearchContributor.java
@@ -1,11 +1,23 @@
 package custom.order.search.contributor;
 
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.FunctionScoreQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.query.Query;
+import com.liferay.portal.search.query.TermQuery;
+import com.liferay.portal.search.query.function.score.ScoreFunctions;
+import com.liferay.portal.search.query.function.score.WeightScoreFunction;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
 import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchContributor;
-
 import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchSettings;
 import custom.order.search.contributor.constants.CustomOrderSearchContributorPortletKeys;
-
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component(
 	immediate = true,
@@ -19,5 +31,35 @@ public class CustomOrderSharedSearchContributor
 	public void contribute(
 		PortletSharedSearchSettings portletSharedSearchSettings) {
 
+		SearchRequestBuilder searchRequestBuilder =
+			portletSharedSearchSettings.getSearchRequestBuilder();
+
+		BooleanQuery booleanQuery = _queries.booleanQuery();
+
+		FunctionScoreQuery functionScoreQuery =
+			_queries.functionScore(booleanQuery);
+
+		TermQuery termQuery = _queries.term(Field.ASSET_TAG_NAMES, "parts");
+		TermQuery termQuery2 = _queries.term(Field.ASSET_TAG_NAMES, "product");
+
+		WeightScoreFunction scoreFunction = _scoreFunctions.weight(100);
+		WeightScoreFunction scoreFunction2 = _scoreFunctions.weight(1);
+
+		functionScoreQuery.addFilterQueryScoreFunctionHolder(
+			termQuery, scoreFunction);
+
+		functionScoreQuery.addFilterQueryScoreFunctionHolder(
+			termQuery2, scoreFunction2);
+
+		searchRequestBuilder.query(functionScoreQuery);
 	}
+
+	@Reference
+	protected Queries _queries;
+
+	@Reference
+	protected ScoreFunctions _scoreFunctions;
+
+	private List<FunctionScoreQuery.FilterQueryScoreFunctionHolder>
+		_filterQueryScoreFunctionHolders = new ArrayList<>();
 }
--- modules/custom-order-search-contributor-portlet/src/main/java/custom/order/search/contributor/portlet/CustomOrderSearchContributorPortlet.java
+++ modules/custom-order-search-contributor-portlet/src/main/java/custom/order/search/contributor/portlet/CustomOrderSearchContributorPortlet.java
@@ -14,7 +14,7 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=category.search",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=CustomOrderSearchContributor",
